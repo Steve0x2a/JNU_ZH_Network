e3b11929aea26ef54a1aad8745eec969cacf378f
0x2a <stevesough@gmail.com>
2021-09-11 16:37:53 +0800

    添加readme

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
 )
 
 func main() {
-	configPath := flag.String("config", "./config.yml", "配置文件地址, 默认当前目录的config.yml")
-	logFlag := flag.Bool("log", false, "日志输出到文件,默认关闭即输出到终端, 打开则输入到文件")
+	configPath := flag.String("config", "./config.yml", "配置文件地址, 默认为\"./config.yml\"")
+	logFlag := flag.Bool("log", false, "日志输出到文件,默认为false即输出到终端, 打开则输入到文件")
 	flag.Parse()
 	utils.LogInit(*logFlag)
 	fmt.Println("使用配置:", *configPath)
